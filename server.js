526062defb456f292e3d72866ef799b569551048
Erin Hamrick <erin.hamrick76@gmail.com>
2023-09-13 10:57:30 -0500

    wrote utility file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const fs = require('fs');
+const api = require('./routes/index')
 
 const PORT = 3001;
 
@@ -15,6 +15,10 @@ app.use(express.static('public'));
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
+app.get('/notes', (req, res) => {
+    res.sendFild(path.join(__dirname, '/public/notes.html'))
+})
 
 
 
